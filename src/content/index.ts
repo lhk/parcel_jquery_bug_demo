79928ef2d34b5359d4f0c742c64be64d29675392
lhk <lars.klein@rwth-aachen.de>
2018-10-25 22:49:34 +0200

    working version

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,6 +1,10 @@
 import $ from "jquery";
+(<any>window).jQuery = $;
+
 import "bootstrap";
 
 $(function () {
   $('[data-toggle="popover"]').popover();
+  console.log("im alive");
+  //$('[data-toggle="popover"]').click(function(){});
 });
