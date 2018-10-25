8c723408faf665839f9d7725a30fc23667b6475b
lhk <lars.klein@rwth-aachen.de>
2018-10-25 23:05:37 +0200

    clean version

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,10 +1,8 @@
 //import $ from "jquery";
 //(<any>window).jQuery = $;
-
 //import "bootstrap";
 
 $(function () {
   $('[data-toggle="popover"]').popover();
   console.log("im alive");
-  //$('[data-toggle="popover"]').click(function(){});
 });
